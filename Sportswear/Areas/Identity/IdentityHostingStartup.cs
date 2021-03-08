1e95a4a8e3dbe78c9e5f56a328905207154b28cc
ChiaJX <iamppjay@gmail.com>
2021-03-09 00:37:12 +0800

    generate identity pages

---
+++ Sportswear/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Sportswear.Areas.Identity.Data;
+using Sportswear.Data;
+
+[assembly: HostingStartup(typeof(Sportswear.Areas.Identity.IdentityHostingStartup))]
+namespace Sportswear.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<SportswearContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("SportswearContextConnection")));
+
+                services.AddDefaultIdentity<SportswearUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<SportswearContext>();
+            });
+        }
+    }
+}
