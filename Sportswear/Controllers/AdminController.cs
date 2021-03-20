14a1b5ba0fc87300b3de8a782278ac78bffec097
KHONG JUN CHENG <junchengkhong@gmail.com>
2021-03-20 11:21:56 +0800

    Login with to different paga with different role Create Staff or Admin UI pages for login, register,contact us and staff management

---
+++ Sportswear/Controllers/AdminController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sportswear.Areas.Identity.Data;
+using Sportswear.Data;
+using Sportswear.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sportswear.Views.Admin
+{
+    public class AdminController : Controller
+    {
+
+        private readonly SportswearContext context;
+        private UserManager<SportswearUser> userManager;
+
+        public AdminController(UserManager<SportswearUser> usrMgr)
+        {
+            userManager = usrMgr;
+        }
+
+
+        public IActionResult AdminPanel()
+        {
+            return View();
+        }
+
+        public IActionResult registerStaff()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> registerStaff(registerStaff user)
+        {
+            
+            if (ModelState.IsValid)
+            {
+
+                SportswearUser webUser = new SportswearUser
+                {
+                    UserName = user.Email,
+                    Email = user.Email,
+                    userRole = user.Role,
+                    EmailConfirmed = true
+                };
+
+                IdentityResult result = await userManager.CreateAsync(webUser, user.Password);
+                if (result.Succeeded)
+                
+                    return RedirectToAction("AdminPanel");
+
+                
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                        ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(user);
+
+        }
+        //public async Task<IActionResult> AdminPanel()
+        //{
+        //    var staffList = from user in context.Users
+        //                          join userRoles in context.UserRoles on user.Id equals userRoles.UserId
+        //                          join role in context.Roles on userRoles.RoleId equals role.Id
+        //                          select new { UserId = user.Id, UserName = user.UserName, RoleId = role.Id, RoleName = role.Name };
+
+        //    return View(await staffList.ToListAsync());
+        //}
+    }
+}
---
+++ Sportswear/Migrations/20210319171134_adduserrole.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sportswear.Migrations
+{
+    public partial class adduserrole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "userRole",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "userRole",
+                table: "AspNetUsers");
+        }
+    }
+}
