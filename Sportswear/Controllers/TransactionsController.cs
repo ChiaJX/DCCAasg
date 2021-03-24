f11b7c675ec2294f6c1526ab525ecbe1da3900e2
KHONG JUN CHENG <junchengkhong@gmail.com>
2021-03-24 23:25:15 +0800

    get user id

--- Sportswear/Controllers/TransactionsController.cs
+++ Sportswear/Controllers/TransactionsController.cs
@@ -21,16 +21,18 @@ namespace Sportswear.Views.Transactions
         private readonly SportswearNewContext _context;
         private readonly ICosmosDbService _cosmosDbService;
 
-        private SignInManager<SportswearUser> SignInManager;
-        private UserManager<SportswearUser> UserManager;
+        private readonly SignInManager<SportswearUser> _SignInManager;
+        private readonly UserManager<SportswearUser> _UserManager;
 
 
         Product prod;
 
-        public TransactionsController(SportswearNewContext context, ICosmosDbService cosmosDbService)
+        public TransactionsController(SportswearNewContext context, ICosmosDbService cosmosDbService, SignInManager<SportswearUser> SignInManager, UserManager<SportswearUser> UserManager)
         {
             _context = context;
             _cosmosDbService = cosmosDbService;
+            _SignInManager = SignInManager;
+            _UserManager = UserManager;
         }
 
         // GET: Transactions
@@ -154,13 +156,18 @@ namespace Sportswear.Views.Transactions
                 transactionId = getTransactionByStatus.transactionId.ToString();
             }
 
-            if (SignInManager.IsSignedIn(User))
+            if (_SignInManager.IsSignedIn(User))
             {
-                var user = from m in UserManager.Users
-                           where m.Id.Equals(UserManager.GetUserId(User))
+                var user = from m in _UserManager.Users
+                           where m.Id.Equals(_UserManager.GetUserId(User))
                            select m.Id;
-            }
 
+                foreach (string Id in user)
+                {
+                    Debug.WriteLine("user ID : " + Id);
+                }
+            }
+            
             List<Transaction> transactionList = _context.Transaction.ToList();
             List<Product> productList = new List<Product>();
             List<string> productNameList = new List<string>();
